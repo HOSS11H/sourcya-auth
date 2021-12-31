3dd28a86a85edfcd71c3f023bdeaadb317cb0ca2
Hossam <56669856+HOSS11H@users.noreply.github.com>
2021-12-31 13:24:15 +0200

    Setting Environment

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,26 @@
+// globalStyles.js
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        text-align: left;
+        font-size: 16px;
+        overflow-x: hidden;
+    }
+    h1, h2, h3, h4, h5, h6 {
+        margin: 0;
+    }
+    p {
+        margin: 0;
+    }
+    a {
+        text-decoration: none;
+    }
+`;
+
+export default GlobalStyle;
